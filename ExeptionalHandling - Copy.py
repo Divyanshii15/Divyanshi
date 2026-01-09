9db2aee6db9b12e6b482328ec83032178c317a88
Divyanshii15 <divyanshiofficial30@gmail.com>
2026-01-09 15:14:59 +0530

    Add files via upload

---
+++ ExeptionalHandling - Copy.py
@@ -0,0 +1,51 @@
+# try:
+#     a=5
+#     b=int(input("enter value: "))
+#     print(a/b)
+# except:
+#     print("no can't be divided by zero")
+
+# try:
+#     x=int(input("enter num: "))
+#     print(10/x)
+# except ZeroDivisionError:
+#     print("can't divided by zero")
+# except ValueError:
+#     print("can't divided by string")
+# else:
+#     print("The input was: ", x)
+# finally:
+#     print("THANK YOU")
+
+        #  INCASE CODE IS ERROR BUT STILL "FINALLY" WILL BE PRINT .)
+
+# # try:
+# y=int(input("Enter Value: "))
+# print(5/y)
+
+# x="niaa"
+# print(x.upper()) 
+                        #FORUPPERCASE
+
+# class AIPA (Exception):
+#     pass 
+# try: 
+#     x=int(input("enter age: "))
+#     if x <=10:
+#         raise AIPA ("not Eligible due to age")
+#     else: 
+#         print("Eligible for Addmission")
+# except AIPA as huhh:
+#     print("reason: ", huhh)
+
+# class E_adhar (Exception):
+#     pass
+# try: 
+#     x=(input("Enter adhar number: "))
+#     if len(x) != 12:
+#         raise E_adhar ("Enter valid Adhar number")
+#     else: 
+#         print("Adhar number Accepted")
+# except E_adhar as xy:
+#     print("adhar number accepted")
+
