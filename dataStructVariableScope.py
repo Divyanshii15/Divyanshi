38fb6f0e1d826b2464fcc029bb5ee719bb4e792d
Divyanshii15 <divyanshiofficial30@gmail.com>
2026-01-09 14:45:55 +0530

    Add files via upload

---
+++ dataStructVariableScope.py
@@ -0,0 +1,67 @@
+#variableScope  #FUNCTION
+
+# attendance="Present"
+# def globals():
+#     attendance="Absent"
+#     print(attendance)
+# globals()
+# print(attendance)
+ 
+                               # global Keyword 
+
+# day="monday"
+# def gk():
+#     global day
+#     day="sunday"
+#     print(day)
+# gk()
+# print(day)
+
+# occasion="diwali"
+# def gk():
+#     global occasion
+#     occasion="HOLI"
+#     print(occasion)
+# gk()
+# print(occasion)
+
+
+# def student():
+#     name="prati"
+#     print(name)
+# student()
+
+# def function1():
+#     y=15
+#     print("value of y in function1:", y)
+# def function2():
+#     z=20
+#     print("value of z in function2:", z)
+# function1()
+# function2()
+
+# college="NSTIW"
+# def course():
+#     course="AIPA"
+#     print("you are Enrolled in", course)
+# print("your college name is", college)
+# course() 
+
+# def factorial(n):
+#     if n ==1:
+#         return 1 
+#     else:
+#         return n * factorial(n-1)
+# num=int(input("Enter Number: ")) 
+# print("Factorial of", num, "is: ", factorial(num))
+
+                          #(practice example )
+
+# num=5
+# if num>3:
+#     print("grater than 3")
+# if num<6:
+#     print("abx")
+
+
+
