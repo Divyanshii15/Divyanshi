a60ff942479f85cf4463ef00f5ef9143a26fefff
Divyanshii15 <divyanshiofficial30@gmail.com>
2026-01-09 15:28:02 +0530

    Add files via upload

---
+++ transferstatement - Copy.py
@@ -0,0 +1,39 @@
+# pass,break is transfer statement
+
+# for i in range (1,10):
+#     pass
+#     for i in range(1,10):
+#         print(i)
+
+# for i in range (1,10):
+#     if i==6:
+#         break
+#     print(i)
+
+# for i in range(1,10):
+#     if i==5:
+#         continue
+#     print(i)
+
+# for i in range(1,11):
+#     if i==4:
+#         continue
+#     if i ==5:
+#         continue
+#     print(i)
+
+# for i in range(1,6):
+#     if i==3:
+#         pass
+#     else:
+#         print(i)
+
+# for i in range(1,11):
+    # if i==4:
+    #     continue
+    # if i==5:
+    #     continue
+    # if i==9:
+    #     break
+    # else:
+    #     print(i)
