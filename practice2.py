13bd65629b561b2341bb50f1ba6656b6dff70cbb
Divyanshii15 <divyanshiofficial30@gmail.com>
2026-01-09 15:31:23 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,30 @@
+# name = "Divyanshi"
+# age = 20
+# phone = 6369685475
+# year = 2025 
+# print(type(name))
+# print(type(phone))
+# print(type(year))
+# print(type(age)) 
+# print("name:",(name))
+# print("age:",(age))
+# print("phone no:",(phone))
+# print("year:",(year))
+# total = age + year 
+# print("total:",total)
+# x = 55
+# y = 5 
+# print(x>y)
+# print(x<y)
+# x = 15 
+# y = "5" 
+# z = int(y) 
+# print(x+z)
+# a=input("your first name: ")
+# b=input("your last name:" )
+# c=a +b
+# # print("hello,", c )
+# a=int(input("entert first number: "))
+# b=int(input("enter second number: "))
+# c= a+ b 
+# print(" finall answer is:",c)
